91f3bdc96e4dba22a3d2e74351167807e5c55f52
Carlos mario ariza barranco <87338487+Carst0716@users.noreply.github.com>
2022-02-27 21:15:38 -0500

    Delete Trading Dashboard directory

--- Trading Dashboard/Códigos/App Django/blog/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View
-
-# Create your views here.
-class BlogListView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'blog_list.html', context)
--- Trading Dashboard/Códigos/Ejemplos/testing.py
+++
@@ -1,7 +0,0 @@
-import investpy
-
-df = investpy.get_stock_historical_data(stock='PMG',
-                                        country='Colombia',
-                                        from_date='01/01/2020',
-                                        to_date='01/01/2022')
-print(df.head())
