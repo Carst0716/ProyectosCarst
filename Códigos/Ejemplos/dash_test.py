aebd6fb9059a20304e6c211ac2af4a11079bb999
unknown <SENA@MABAPRRATFSD025.FormacionSena.RED>
2022-03-05 11:23:59 -0500

    Dashboard_callback

--- Códigos/Ejemplos/dash_test.py
+++ Códigos/Ejemplos/dash_test.py
@@ -1,26 +1,31 @@
 from dataclasses import dataclass
 from os import stat
-from readline import get_history_item
+#from readline import get_history_item
 from turtle import color, end_fill
-from cv2 import line
+#from cv2 import line
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-from iexfinance.stocks import get_historical_data
+#from iexfinance.stocks import get_historical_data
 import datetime
 from dateutil.relativedelta import relativedelta
 import yfinance as yf
 import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
+from datetime import date
+from datetime import datetime
+
+#Día actual
+today = date.today()
 
 
 #FASE 1: Obtención de los Datos
 
 Activo="COP=X"
 FechaIni="2019-01-01"
-FechaFin="2022-02-27"
+FechaFin=today
 
 
 df=yf.download(Activo,FechaIni,FechaFin)
@@ -29,6 +34,12 @@ print(df.info())
 trace_close=go.Scatter(x=list(df.index),
                        y=list(df.Close),
                        name="Close",
+                       line=dict(color="#f44242"))
+
+
+trace_high=go.Scatter(x=list(df.index),
+                       y=list(df.High),
+                       name="High",
                        line=dict(color="#f44242"))
 
 data=[trace_close]
@@ -42,6 +53,16 @@ app= dash.Dash(__name__)
 
 app.layout=html.Div([
 
+html.H1(children="DASHBOARD TEST"),
+html.Label("DASH GRAPH"),
+html.Div([
+dcc.Input(
+    id="stock-input",
+    placeholder="Enter a stock to be charter",
+    type="text",
+    value="SPY"
+)]),
+
 html.Div(
     [
         html.H2("Stock App"),
@@ -50,36 +71,54 @@ html.Div(
     ], className="banner"
 ),
 
-html.H1(children="DASHBOARD TEST"),
-html.Label("DASH GRAPH"),
+
 html.Div(
-dcc.Input(
-    id="Stock-input",
-    placeholder="Enter a stock to be charter",
-    type="text",
-    value=""
+    [
+        html.Div([
+            dcc.Graph(
+                id="graph_close",
+                figure={
+                    "data":[trace_close],
+                    "layout":{
+                        "title":"Close Graph"
+                    }
+                }
+
+            )
+        ], className="six columns"),
+
+        html.Div([
+            dcc.Graph(
+                id="graph_high",
+                figure={
+                    "data":[trace_high],
+                    "layout":{
+                        "title":"High Graphs"
+                    }
+                }
+
+            )
+        ], className="six columns")
+    ], className="row"
 )
-),
+,
 
 html.Div(dcc.Dropdown(
     options=[
         {'label':"Candlestick","value":"Candlestick"},
         {"label":"Line","value":"Line"}
     ]
-)),
+))
 
-html.Div(
-    dcc.Graph(id="Stock Chart",
-              figure=fig)
-    )
 ])
 
+#pp.css.append_css({"external_url":"https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
-"""app.css.append_css(
-    {
-        "external_url":"https://"
-    }
-)"""
+
+@app.callback(dash.dependencies.Output("graph_close","figure")
+            [dash.dependencies.Input("stock-input","value")]
+            )
+
 
 if __name__=="__main__":
     app.run_server(debug=True)
