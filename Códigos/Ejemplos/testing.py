17e3b957880ad6334e07c1dc5586a6e8b76dd90f
unknown <SENA@MABAPRRATFSD025.FormacionSena.RED>
2022-03-12 07:16:44 -0500

    Fase Final Test

--- Códigos/Ejemplos/testing.py
+++ Códigos/Ejemplos/testing.py
@@ -1,7 +1,63 @@
-import investpy
+import pandas_datareader.data as web
+import datetime
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
 
-df = investpy.get_stock_historical_data(stock='PMG',
-                                        country='Colombia',
-                                        from_date='01/01/2020',
-                                        to_date='01/01/2022')
-print(df.head())
+#start = datetime.datetime(2021, 1, 1)
+#end = datetime.datetime.now()
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.Div(children='''
+        Introduzca un Activo:
+    '''),
+    dcc.Input(id='input', value='SPY', type='text'),
+    dcc.Input(id='start', value='2021-01-01', type='text'),
+    dcc.Input(id='end', value='2022-01-01', type='text'),
+    html.Div(id='output-graph'),
+])
+
+
+@app.callback(
+    Output('output-graph', 'children'),
+    [Input("start", "value"),
+    Input("end", "value"),
+    Input('input', 'value')]
+)
+def update_value(input_data):
+    df = web.DataReader(input_data, 'yahoo', start, end)
+    df.reset_index(inplace=True)
+    df.set_index("Date", inplace=True)
+    #df = df.drop("Symbol", axis=1)
+
+    return html.Div([dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},
+            ],
+            'layout': {
+                'title': input_data
+            }
+        }
+    ),
+        dcc.Graph(
+                id='example-graph',
+                figure={
+                    'data': [
+                        {'x': df.index, 'y': df.Open, 'type': 'line', 'name': input_data},
+                    ],
+                    'layout': {
+                        'title': input_data
+                    }
+                }
+            )
+
+    ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
